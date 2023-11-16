48c61f07e7e4bab2a1c6274be9320a8798b35847
minhajsvn <minhajrafeeq7@gmail.com>
2023-11-16 15:22:59 +0530

    Module 1 Navbar

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from  "react";
+import Logo from '../Logo/Logo';
+import Search from '../Search/Search';
+import Button from '../Button/Button';
+import styles from '../Navbar/Navbar.module.css'
+
+export default function Navbar(){
+
+    return (
+        <nav className={styles.navbar}>
+            <a href="/">
+                <Logo />
+            </a>
+            <Search placeholder="Search a album of your choice" />
+            <Button>Give Feedback</Button>
+        </nav>
+    )
+}
